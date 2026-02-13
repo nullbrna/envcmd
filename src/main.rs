071aedb5402dc47681c6ec435a81acbf7d348dc8
nullbrna <252729190+nullbrna@users.noreply.github.com>
2026-02-13 14:44:40 +0000

    feat: dynamic kind & synchronous running

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::env::current_dir;
+use std::env::vars;
 use std::io::BufRead;
 use std::io::BufReader;
+use std::path::PathBuf;
 use std::process::Command;
 use std::process::Stdio;
 
@@ -17,22 +20,37 @@ macro_rules! log {
     eprintln!("\x1b[31mE.\x1b[0m {}", format_args!($($arg)*));
   }};
   (COLOUR, $idx:expr, $($arg:tt)*) => {{
-    println!("{}{}.\x1b[0m {}", COLOUR[$idx % COLOUR.len()], $idx, format_args!($($arg)*));
-  }};
-  (ABORT, $($arg:tt)*) => {{
-    eprintln!("\x1b[31mE.\x1b[0m {}", format_args!($($arg)*));
-    std::process::exit(1);
+    let colour = COLOUR[$idx % COLOUR.len()];
+    println!("{}{}.\x1b[0m {}", colour, $idx, format_args!($($arg)*));
   }};
 }
 
-fn run_command(idx: usize, cmd: &str) {
-  let merged_cmd = format!("{cmd} 2>&1");
-  log!(COLOUR, idx, "[+] {cmd}");
+#[derive(PartialEq)]
+enum Kind {
+  Directory,
+  Branch,
+}
+
+fn kind_from_str(source: &str) -> Option<Kind> {
+  if source.eq_ignore_ascii_case("dir") {
+    return Some(Kind::Directory);
+  } else if source.eq_ignore_ascii_case("bra") {
+    return Some(Kind::Branch);
+  }
+
+  None
+}
+
+fn run_command(idx: usize, command: &str) {
+  let cmd = format!("{command} 2>&1");
+  let pipeout = Stdio::piped();
+
+  log!(COLOUR, idx, "[+] {command}");
 
   let Ok(mut child) = Command::new("sh")
     .arg("-c")
-    .arg(&merged_cmd)
-    .stdout(Stdio::piped())
+    .arg(&cmd)
+    .stdout(pipeout)
     .spawn()
   else {
     log!(ERROR, "[{idx}] unable to start");
@@ -53,10 +71,75 @@ fn run_command(idx: usize, cmd: &str) {
     return;
   };
 
-  log!(COLOUR, idx, "[-] {cmd}");
+  log!(COLOUR, idx, "[-] {command}");
+}
+
+fn is_directory(target: &str) -> bool {
+  let is_match = |path: PathBuf| {
+    path
+      .file_name()
+      .is_some_and(|name| name.eq_ignore_ascii_case(target))
+  };
+
+  current_dir().is_ok_and(is_match)
+}
+
+fn is_branch(target: &str) -> bool {
+  let Ok(child) = Command::new("git")
+    .args(["rev-parse", "--abbrev-ref", "HEAD"])
+    .output()
+  else {
+    return false;
+  };
+
+  if child.status.success() {
+    return String::from_utf8_lossy(&child.stdout)
+      .trim()
+      .eq_ignore_ascii_case(target);
+  }
+
+  false
+}
+
+fn start(kind: Kind, target: &str, commands: Vec<&str>) {
+  let matches = match kind {
+    Kind::Directory => is_directory(target),
+    Kind::Branch => is_branch(target),
+  };
+
+  if !matches {
+    return;
+  }
+
+  log!(INFO, "[+] {target}");
+  for (idx, cmd) in commands.iter().enumerate() {
+    run_command(idx, cmd);
+  }
+
+  log!(INFO, "[-] {target}")
 }
 
 fn main() {
-  run_command(0, "echo 'hello, world!'");
-  run_command(1, "echo 'goodbye, world!'");
+  const START: &str = "EC_";
+  const SPACE: &str = "_";
+  const DELIM: &str = ",";
+
+  for (key, value) in vars() {
+    let Some(key) = key.strip_prefix(START) else {
+      continue;
+    };
+
+    let mut parts = key.split(SPACE);
+    let commands = value.split(DELIM).collect();
+    let (kind, target) = (parts.next(), parts.next());
+
+    if let Some(Some(kind)) = kind.map(kind_from_str)
+      && let Some(target) = target
+    {
+      start(kind, target, commands);
+      continue;
+    }
+
+    log!(ERROR, "unknown format: {key}");
+  }
 }
