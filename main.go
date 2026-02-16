a4baf8bef197ec514d82c3345f198780eaf73940
nullbrna <252729190+nullbrna@users.noreply.github.com>
2026-02-16 20:27:56 +0000

    feat: rewrite complete

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "os"
+    "os/exec"
+    "path/filepath"
     "strings"
+    "sync"
 )
 
-var COLOURS = []string{
+var colours = []string{
     "\x1b[94m", // Bright Blue
     "\x1b[95m", // Bright Magenta
     "\x1b[96m", // Bright Cyan
+}
+
+type Kind string
+
+const (
+    dir Kind = "DIR"
+    bra Kind = "BRA"
+)
+
+func (this Kind) Valid() bool {
+    return this == dir || this == bra
 }
 
 func logInfo(format string, args ...any) {
@@ -24,60 +39,117 @@ func logError(format string, args ...any) {
 
 func logColour(idx int, format string, args ...any) {
     text := fmt.Sprintf(format, args...)
-    fmt.Printf("[%s%d\x1b[0m] %s\n", COLOURS[idx%len(COLOURS)], idx, text)
+    fmt.Printf("[%s%d\x1b[0m] %s\n", colours[idx%len(colours)], idx, text)
 }
 
-func validation(parts []string) error {
-    if len(parts) == 4 && parts[1] != "ASYNC" {
-        return fmt.Errorf("expected <ASYNC> instead of %s", parts[1])
-    }
-
-    var index int
-    if len(parts) == 4 {
-        index = 2
-    } else {
-        index = 1
-    }
-
-    if parts[index] != "DIR" && parts[index] != "BRA" {
-        return fmt.Errorf("unknown <KIND> value of %s", parts[index])
+func validation(segments []string, kind Kind, async bool) error {
+    if async && segments[1] != "ASYNC" {
+        return fmt.Errorf("expected <ASYNC> instead of %s", segments[1])
+    } else if !kind.Valid() {
+        return fmt.Errorf("unknown <KIND> value of %s", kind)
     }
 
     return nil
 }
 
-func main() {
-    envs := os.Environ()
+func isDirectory(target string) bool {
+    dir, err := os.Getwd()
+    if err != nil {
+        logError("retrieving directory: %s", err)
+        return false
+    }
 
-    for i := 0; i < len(envs); i++ {
-        parts := strings.Split(envs[i], "=")
-        if len(parts) != 2 {
+    return strings.EqualFold(filepath.Base(dir), target)
+}
+
+func isBranch(target string) bool {
+    out, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
+    if err != nil {
+        logError("retrieving branch: %s", err)
+        return false
+    }
+
+    str := string(out)
+    return strings.EqualFold(strings.TrimSpace(str), target)
+}
+
+func run(wg *sync.WaitGroup, idx int, command string) {
+    defer wg.Done()
+
+    logInfo("[+] %s", command)
+    cmd := exec.Command("sh", "-c", command+" "+"2>&1")
+
+    stdout, err := cmd.StdoutPipe()
+    if err != nil {
+        logError("[%d] reading stdout: %v", idx, err)
+        return
+    }
+
+    if err := cmd.Start(); err != nil {
+        logError("[%d] unable to start: %v", idx, err)
+        return
+    }
+
+    reader := bufio.NewScanner(stdout)
+    for reader.Scan() {
+        logColour(idx, "%s", reader.Text())
+    }
+
+    if err := cmd.Wait(); err != nil {
+        logError("[%d] awaiting completion: %v", idx, err)
+        return
+    }
+
+    logInfo("[-] %s", command)
+}
+
+func start(kind Kind, target string, async bool, commands []string) {
+    if (kind == dir && !isDirectory(target)) || (kind == bra && !isBranch(target)) {
+        return
+    }
+
+    var wg sync.WaitGroup
+    for idx := 0; idx < len(commands); idx++ {
+        wg.Add(1)
+        cmd := commands[idx]
+
+        if async {
+            go run(&wg, idx, cmd)
             continue
         }
 
-        key, val := parts[0], parts[1]
+        run(&wg, idx, cmd)
+    }
+
+    wg.Wait()
+}
+
+func main() {
+    envs := os.Environ()
+
+    for idx := 0; idx < len(envs); idx++ {
+        segments := strings.SplitN(envs[idx], "=", 2)
+
+        key, val := segments[0], segments[1]
         if !strings.HasPrefix(key, "EVC_") {
             continue
         }
 
-        parts = strings.Split(key, "_")
-        length := len(parts)
+        segments = strings.Split(key, "_")
+        length := len(segments)
 
         if length != 3 && length != 4 {
             logError("malformed key: %s", key)
             continue
         }
 
-        if err := validation(parts); err != nil {
+        kind, target, async := Kind(segments[length-2]), segments[length-1], length == 4
+        if err := validation(segments, kind, async); err != nil {
             logError("invalid key: %s", err)
             continue
         }
 
-        async := length == 4
-        logInfo("%s = %v", key, async)
-
-        logColour(0, "%s", val)
-        logColour(1, "%s", val)
-        logColour(2, "%s", val)
+        commands := strings.Split(val, ",")
+        start(kind, target, async, commands)
     }
 }
